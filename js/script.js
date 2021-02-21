dd76cc59fa4d3d64619278c1d86950853f422f74
fatma khamis <fatmaali#hotmail.com>
2021-02-22 02:26:38 +0300

    add function getting akan name

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 <script>
 
+function getName () {
+  let yearOfBirth = document.getElementById("year-input").value;
+  let monthOfBirth = Number(document.getElementById("month-input").value);
+  let dayOfBirth = Number(document.getElementById("day-input").value);
+  let genders = document.getElementsByName("gender");
+
+  // function to get gender
+  function getGender () {
+    for (let gender of genders){
+      if (gender.checked){
+        return gender.value;
+      }
+    }
+  }
+
+  let myGenderValue = getGender();
+  console.log(myGenderValue);
 // validation functions
   function monthValidator () {
     if (monthOfBirth < 1 || monthOfBirth > 12) {
